575c27e46c0ffb834ca9382659b58b65067c21c7
Derek Anderson <derek@akash.network>
2021-05-24 17:58:19 -0500

    update requirements for setting up environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,14 @@
 #!/bin/sh -l
-echo "::set-output name=version::"akash version
+export AKASH_NET="https://raw.githubusercontent.com/ovrclk/net/master/mainnet"
+export AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")"
+export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)"
+
+echo "::set-output name=cli-version::"$(akash version)
+echo "::set-output name=network::"$AKASH_NET
+echo "::set-output name=network-version::"$AKASH_VERSION
+echo "::set-output name=network-node::"$AKASH_NODE
+
+
+echo $1 > /tmp/akash_wallet_key.txt
+
+yes $2 | akash keys import $3 /tmp/akash_wallet_key.txt
