b90d89465cf1e6ec209d1e449a78194b9bb08efd
Derek Anderson <derek@akash.network>
2021-05-24 23:35:29 -0500

    .

--- main.js
+++ main.js
@@ -6,11 +6,8 @@ fs.writeFileSync("/tmp/akash_wallet_key.txt", process.argv[2]);
 fs.writeFileSync("/tmp/akash_wallet_key_pass.txt", process.argv[3]);
 
 exec(
-  `akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt < "${process.argv[3]}"` , (err, stdout, stderr){
-      console.log(
-          err,
-          stdout,
-          stderr
-      )
+  `akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt < "${process.argv[3]}"`,
+  (err, stdout, stderr) => {
+    console.log(err, stdout, stderr);
   }
 );
