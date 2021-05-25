85e356a3c98b0edf5cd1f9145cf94a2a6a10a879
Derek Anderson <derek@akash.network>
2021-05-24 22:47:58 -0500

    .

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 #!/usr/bin/env node
 const { exec } = require("child_process");
 
-console.log("hello world");
 console.log(process.argv);
 console.log(process.argv[3].length);
 console.log(process.argv[3].length);
 
-exec(
-  `./script.sh "${process.argv[2]}" ${process.argv[3]}`,
-  (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(stdout);
+exec(`ls -a `, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
   }
-);
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(stdout);
+});
