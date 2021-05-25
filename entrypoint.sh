ed5e21a0983cfcbc196c836f27c6f299c148f167
Derek Anderson <derek@akash.network>
2021-05-24 20:48:46 -0500

    ecgo enbtry

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ echo "::set-output name=network::"$AKASH_NET
 echo "::set-output name=network-version::"$AKASH_VERSION
 echo "::set-output name=network-node::"$AKASH_NODE
 
+echo $1 $2 $3
 
 echo "$1" >> /tmp/akash_wallet_key.txt
 
 yes $2 | akash keys import $3 /tmp/akash_wallet_key.txt
 
-akash \
-  keys show "$3" -a
+akash keys show "$3" -a
