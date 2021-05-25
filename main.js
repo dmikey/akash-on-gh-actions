449d82ff89767abbb6a62245b15cd0f950825499
Derek Anderson <derek@akash.network>
2021-05-24 23:03:01 -0500

    .

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env node
 const { exec } = require("child_process");
+const fs = require("fs");
+fs.writeFileSync("/tmp/akash_wallet_key.txt", process.argv[2]);
+
+// console.log(process.argv);
+// console.log(process.argv[3].length);
+// console.log(process.argv[3].length);
+
+exec(`akash version `, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(stdout);
+});
 
 exec(
-  `echo $(cat <<-END ${process.argv[2]} END ) >> /tmp/akash_wallet_key.txt`,
+  `yes ${process.argv[3]} | akash keys import /tmp/akash_wallet_key.txt"`,
   (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
@@ -12,29 +30,6 @@ exec(
       console.log(`stderr: ${stderr}`);
       return;
     }
-    exec(`akash version`, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      exec(
-        `yes ${process.argv[3]} | akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt`,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-          }
-          if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-          }
-          console.log(stdout);
-        }
-      );
-    });
+    console.log(stdout);
   }
 );
