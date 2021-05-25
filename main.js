4335617c973b828f4ffe82f0099c9cf7f9c80d9a
Derek Anderson <derek@akash.network>
2021-05-24 22:28:34 -0500

    run shell

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ console.log(process.argv);
 console.log(process.argv[3].length);
 console.log(process.argv[3].length);
 
-exec("echo " + process.argv[3], (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+exec(
+  `./entrypoint.sh ${process.argv[2]} ${process.argv[3]}`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(stdout);
   }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(stdout.trim().length);
-});
+);
