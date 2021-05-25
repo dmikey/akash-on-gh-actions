7067d1ced59bb85f08eaaea452eb9d209ead37ce
Derek Anderson <derek@akash.network>
2021-05-25 16:00:37 -0500

    b64

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 
 fs.writeFileSync("/tmp/key.key", process.env.WALLET_KEY);
 exec(
-  `wallet_pass=${process.env.WALLET_KEY_PASS.split("")
-    .reverse()
-    .join("")}; yes $wallet_pass | akash keys import "TEST_KEY" /tmp/key.key`,
+  `wallet_pass=${Buffer.from(process.env.WALLET_KEY_PASS, "base64").toString(
+    "ascii"
+  )}; yes $wallet_pass | akash keys import "TEST_KEY" /tmp/key.key`,
   (err, stdout, stderr) => {
     console.log({ stdout, stderr });
   }
