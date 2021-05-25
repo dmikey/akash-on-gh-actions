0de70ac984237ee0390383a62ca5de91b045864f
Derek Anderson <derek@akash.network>
2021-05-24 22:22:31 -0500

    main

--- main.js
+++ main.js
@@ -3,3 +3,16 @@
 console.log("hello world");
 console.log(process.argv);
 console.log(process.argv[3].length);
+console.log(process.argv[3].length);
+
+exec("echo " + process.argv[3], (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`${stdout}`.length);
+});
