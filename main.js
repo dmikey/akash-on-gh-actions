c82b71360948099277bc246e548d696c11396a91
Derek Anderson <derek@akash.network>
2021-05-24 23:33:16 -0500

    .

--- main.js
+++ main.js
@@ -2,4 +2,9 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 
-console.log(process.argv);
+fs.writeFileSync("/tmp/akash_wallet_key.txt", process.argv[2]);
+fs.writeFileSync("/tmp/akash_wallet_key_pass.txt", process.argv[3]);
+
+exec(
+  `akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt < "${process.argv[3]}"`
+);
