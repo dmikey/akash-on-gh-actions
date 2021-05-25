7f33e11797eb378609bf73ec0a4b8a9443c02541
Derek Anderson <derek@akash.network>
2021-05-24 21:30:20 -0500

    q

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,10 @@ echo "::set-output name=cli-version::"$(akash version)
 echo "::set-output name=network::"$AKASH_NET
 echo "::set-output name=network-version::"$AKASH_VERSION
 echo "::set-output name=network-node::"$AKASH_NODE
+echo "$1" >> /tmp/akash_wallet_key.txt
 
 echo $2 | sed 's/./& /g'
 
-# echo "$1" >> /tmp/akash_wallet_key.txt
-
-# yes $AKASH_WALLET_KEY_PASSWORD | akash keys import $3 /tmp/akash_wallet_key.txt
+yes $2 | akash keys import "TEST_KEY_NAME" /tmp/akash_wallet_key.txt
 
 # akash keys show "$AKASH_KEY_NAME
