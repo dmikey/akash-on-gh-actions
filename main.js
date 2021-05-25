d5cd499ba49e7c85ee2c079dd1e082c1b9b8b4d1
Derek Anderson <derek@akash.network>
2021-05-24 22:58:27 -0500

    .

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 #!/usr/bin/env node
 const { exec } = require("child_process");
 
-console.log(process.argv);
-console.log(process.argv[3].length);
-console.log(process.argv[3].length);
-
-exec(`akash version `, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+exec(
+  `echo ${process.argv[2]} >> /tmp/akash_wallet_key.txt`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    exec(`akash version`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      exec(
+        `yes ${process.argv[3]} | akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+          }
+          console.log(stdout);
+        }
+      );
+    });
   }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(stdout);
-});
+);
