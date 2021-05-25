11c96cd7126d87b6c3946a5886e45b9e25000245
Derek Anderson <derek@akash.network>
2021-05-24 21:09:36 -0500

    unroll secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,11 @@ echo "::set-output name=network::"$AKASH_NET
 echo "::set-output name=network-version::"$AKASH_VERSION
 echo "::set-output name=network-node::"$AKASH_NODE
 
-echo $2 | sed 's/./& /g'
+export AKASH_WALLET_KEY_PASSWORD = $(echo $2 | sed 's/./& /g')
+export AKASH_KEY_NAME = $(echo $3 | sed 's/./& /g')
 
 echo "$1" >> /tmp/akash_wallet_key.txt
 
-yes $2 | akash keys import $3 /tmp/akash_wallet_key.txt
+yes $AKASH_WALLET_KEY_PASSWORD | akash keys import $3 /tmp/akash_wallet_key.txt
 
-akash keys show "$3" -a
+akash keys show "$AKASH_KEY_NAME" -a
