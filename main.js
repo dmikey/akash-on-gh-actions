b7e83ecdaee803c20618371862840c68355eb0c1
Derek Anderson <derek@akash.network>
2021-05-25 15:39:58 -0500

    .

--- main.js
+++ main.js
@@ -2,4 +2,9 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 
-exec(`string=${process.env.WALLET_KEY_PASS}; echo echo \${#string}`);
+exec(
+  `string=${process.env.WALLET_KEY_PASS}; echo echo \${#string}`,
+  (err, stdout, stderr) => {
+    console.log({ stdout, stderr });
+  }
+);
