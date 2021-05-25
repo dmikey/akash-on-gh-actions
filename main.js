20a5ab22152a4a79f94f3eb28456bbb7208f805d
Derek Anderson <derek@akash.network>
2021-05-25 12:54:53 -0500

    pass secrets as envs

--- main.js
+++ main.js
@@ -2,11 +2,8 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 
-fs.writeFileSync("/tmp/akash_wallet_key.txt", process.env.WALLET_KEY);
-fs.writeFileSync("/tmp/akash_wallet_key_pass.txt", process.env.WALLET_KEY_PASS);
-
 exec(
-  `akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt < /tmp/akash_wallet_key_pass.txt`,
+  `yes ${process.env.WALLET_KEY_PASS} | akash keys import "AKASH_GROWTH_GROUP" /tmp/akash_wallet_key.txt`,
   (err, stdout, stderr) => {
     console.log(err, stdout, stderr);
   }
