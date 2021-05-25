58ef1f16e3846bd91d3d9851f888dfbbc80016ce
Derek Anderson <derek@akash.network>
2021-05-24 21:19:49 -0500

    secrets testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,10 @@ echo "::set-output name=network::"$AKASH_NET
 echo "::set-output name=network-version::"$AKASH_VERSION
 echo "::set-output name=network-node::"$AKASH_NODE
 
-export AKASH_WALLET_KEY_PASSWORD=$(echo $2 | sed 's/./&/g')
-export AKASH_KEY_NAME=$(echo $3 | sed 's/./&/g')
+echo $1
 
-echo $AKASH_WALLET_KEY_PASSWORD
+# echo "$1" >> /tmp/akash_wallet_key.txt
 
-echo "$1" >> /tmp/akash_wallet_key.txt
+# yes $AKASH_WALLET_KEY_PASSWORD | akash keys import $3 /tmp/akash_wallet_key.txt
 
-yes $AKASH_WALLET_KEY_PASSWORD | akash keys import $3 /tmp/akash_wallet_key.txt
-
-akash keys show "$AKASH_KEY_NAME" -a
+# akash keys show "$AKASH_KEY_NAME" -a
